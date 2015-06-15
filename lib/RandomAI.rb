afba5b6739a7a4894477197fce1b2618b9fa371e
Jonathan Graham <jonathanpgraham@yahoo.co.uk>
2015-06-15 17:38:24 -0500

    Create RandomAI.rb

---
+++ lib/RandomAI.rb
@@ -0,0 +1,20 @@
+class RandomAI
+
+	attr_reader :name
+
+	def initialize()
+		@name = RandomAI
+	end
+
+	def generate_random_position(board)
+		rand(board.board.length)
+	end
+
+	def determine_move(board)
+		position = generate_random_position(board)
+		until board.empty_space?(position) || board.tie? do
+			position = generate_random_position(board)
+		end
+		position
+	end
+end
