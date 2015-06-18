8c4749819a30e30a576d3d9e413bf9578e68fe2f
Jonathan Graham <jonathanpgraham@yahoo.co.uk>
2015-06-18 17:24:01 -0500

    Update WebReadWrite.rb
    refactored

--- lib/WebReadWrite.rb
+++ lib/WebReadWrite.rb
@@ -7,61 +7,87 @@ require '/Users/jonathangraham/TTT_TDD/lib/Game'
 require '/Users/jonathangraham/TTT_TDD/lib/WebReadWrite'
 require 'sinatra'
 
+class WebPlay
+
+  attr_accessor :player1, :player2, :board, :game, :current_player
+
+  def initialize(players_info, board_size, win_size)
+    @player1 = player_setup(players_info, 0)
+    @player2 = player_setup(players_info, 1)
+    @board = Board.new(board_size, win_size)
+    @game = game_setup
+    @current_player = nil
+  end
+
+  def player_setup(players_info, number)
+    players = players_info.map do |player|
+      if player[0] == 'Human'
+        Human.new(player[1])
+      elsif player[2] == 'Easy'
+        SequentialAI.new('Computer_Easy')
+      elsif player[2] == 'Moderate'
+        RandomAI.new('Computer_Moderate')
+      elsif player[2] == 'Hard'
+        NegamaxAI.new('Computer_Hard')
+      end
+    end
+    players[number]
+  end
+
+  def game_setup
+    Game.new(board, player1, player2)
+  end
+
+  def change_player
+    if current_player == player1 
+      @current_player = player2 
+    else
+      @current_player = player1
+    end
+  end
+
+end
+
 class WebReadWrite < Sinatra::Base
 
-  BOARD = nil
-  GAME = nil
-  PLAYER1 = nil
-  PLAYER2 = nil
-  BOARD_SIZE = nil
-  WIN_SIZE = nil
+  WEB_PLAY = nil
   POSITION = nil
-  CURRENT_PLAYER = nil
+  LOCALS = nil
 
-
+  get '/' do
+    redirect '/new_game'
+  end
+  
   get '/new_game' do
   	erb :new_game_form
   end
 
   post '/new_game' do
-  	PLAYER1 = params[:player1]
-    PLAYER2 = params[:player2]
-    player1_name = params[:player1_name]
-    player2_name = params[:player2_name]
-    player1_level = params[:player1_level]
-    player2_level = params[:player2_level]
-    BOARD_SIZE = params[:board_size].to_i
-    WIN_SIZE = params[:win_size].to_i
-    if WIN_SIZE > BOARD_SIZE
-      WIN_SIZE = BOARD_SIZE
-    end    
-    
-
-    if PLAYER1 == 'Human'
-    	PLAYER1 = Human.new(player1_name)
-    elsif player1_level == 'Easy'
-    	PLAYER1 = SequentialAI.new('Computer_Easy')
-    elsif player1_level == 'Moderate'
-    	PLAYER1 = RandomAI.new('Computer_Moderate')
-    elsif player1_level == 'Hard'
-    	PLAYER1 = NegamaxAI.new('Computer_Hard')
+    player1_info = [params[:player1], params[:player1_name], params[:player1_level]]
+    player2_info = [params[:player2], params[:player2_name], params[:player2_level]]
+    players_info = [player1_info, player2_info]
+    board_size = [params[:board_size].to_i, 2].max
+    win_size = [params[:win_size].to_i, 2].max
+    if win_size > board_size
+      win_size = board_size
     end
 
-	  if PLAYER2 == 'Human'
-    	PLAYER2 = Human.new(player2_name)
-    elsif player2_level == 'Easy'
-    	PLAYER2 = SequentialAI.new('Computer_Easy')
-    elsif player2_level == 'Moderate'
-    	PLAYER2 = RandomAI.new('Computer_Moderate')
-    elsif player2_level == 'Hard'
-    	PLAYER2 = NegamaxAI.new('Computer_Hard')
-    end
+    WEB_PLAY = WebPlay.new(players_info, board_size, win_size)
+    LOCALS = {
+      'board' => WEB_PLAY.board.board,
+      'board_index' => WEB_PLAY.board.index_board, 
+      'player1' => WEB_PLAY.player1, 
+      'player2' => WEB_PLAY.player2, 
+      'board_size' => WEB_PLAY.board.rows.length, 
+      'win_size' => WEB_PLAY.board.x_in_a_row
+    }
 
-    
-    BOARD = Board.new(BOARD_SIZE, WIN_SIZE)
-    GAME = Game.new(BOARD, PLAYER1, PLAYER2)
-  
-    if PLAYER1.class != Human && PLAYER2.class != Human
+
+    redirect '/start_game'
+  end
+
+  get '/start_game' do
+    if WEB_PLAY.player1.class != Human && WEB_PLAY.player2.class != Human
       redirect '/computer_vs_computer'
     else
   	 redirect '/make_move'
@@ -69,50 +95,44 @@ class WebReadWrite < Sinatra::Base
   end
 
   get '/make_move' do
-    if BOARD.game_end?
-      redirect '/game_over'
-    end
-    if CURRENT_PLAYER == PLAYER1
-      CURRENT_PLAYER = PLAYER2
-    else
-      CURRENT_PLAYER = PLAYER1
-    end
-    if CURRENT_PLAYER.class != Human
-      POSITION = CURRENT_PLAYER.determine_move(BOARD) 
-      GAME.make_move(BOARD, POSITION)
+    redirect '/game_over' if WEB_PLAY.board.game_end?   
+    current_player = WEB_PLAY.change_player
+    if current_player.class != Human
+      POSITION = current_player.determine_move(WEB_PLAY.board) 
+      WEB_PLAY.game.make_move(WEB_PLAY.board, POSITION)
       redirect '/make_move'
     else
-      if BOARD.board.count(' ') == BOARD.board.length
-        @message = "First go! #{CURRENT_PLAYER.name} to start."
+      if WEB_PLAY.board.board.count(' ') == WEB_PLAY.board.board.length
+        @message = "First go! #{current_player.name} to start."
       else
-        @message = "Last play was in position #{POSITION}. #{CURRENT_PLAYER.name} take your turn."
+        @message = "Last play was in position #{POSITION}. #{current_player.name} take your turn."
       end
-      erb :make_move_form, :locals => {'board' => BOARD.board,'board_index' => BOARD.index_board, 'player1' => PLAYER1, 'player2' => PLAYER2, 'board_size' => BOARD_SIZE, 'win_size' => WIN_SIZE}
+      erb :make_move_form, :locals => LOCALS
     end  
   end
 
   post '/make_move' do
     POSITION = params[:move].to_i
-    if BOARD.valid_space?(POSITION) == false
-      redirect '/try_again'
+    if WEB_PLAY.board.valid_space?(POSITION) == false
+      redirect '/try_again/'
     else
-      GAME.make_move(BOARD, POSITION)
+      WEB_PLAY.game.make_move(WEB_PLAY.board, POSITION)
     end
     redirect '/make_move'
   end
 
   get '/try_again' do
     @message = "Position #{POSITION} is not valid. Choose an available space!"
-    erb :make_move_form, :locals => {'board' => BOARD.board,'board_index' => BOARD.index_board, 'player1' => PLAYER1, 'player2' => PLAYER2, 'board_size' => BOARD_SIZE, 'win_size' => WIN_SIZE}
+    erb :make_move_form, :locals => LOCALS
   end
 
   get '/game_over' do
-    if BOARD.tie?
+    if WEB_PLAY.board.tie?
       @message = "Game was a tie. Would you like to play again?"
     else
-      @message = "#{CURRENT_PLAYER.name} won! Would you like to play again?"
+      @message = "#{WEB_PLAY.current_player.name} won! Would you like to play again?"
     end
-    erb :game_over_form, :locals => {'board' => BOARD.board,'board_index' => BOARD.index_board, 'player1' => PLAYER1, 'player2' => PLAYER2, 'board_size' => BOARD_SIZE, 'win_size' => WIN_SIZE} 
+    erb :game_over_form, :locals => LOCALS
   end
 
   post '/game_over' do
@@ -123,31 +143,23 @@ class WebReadWrite < Sinatra::Base
     end
   end
 
-  get '/computer_vs_computer' do
-    if BOARD.game_end?
-      redirect '/game_over'
-    end
-    if CURRENT_PLAYER == PLAYER1
-      CURRENT_PLAYER = PLAYER2
-      @message = "#{PLAYER2.name} (O) to play next"
-    else
-      CURRENT_PLAYER = PLAYER1
-      @message = "#{PLAYER1.name} (X) to play next"
-    end
-    erb :computer_vs_computer_form, :locals => {'board' => BOARD.board,'board_index' => BOARD.index_board, 'player1' => PLAYER1, 'player2' => PLAYER2, 'board_size' => BOARD_SIZE, 'win_size' => WIN_SIZE} 
+  get '/computer_vs_computer' do   
+    redirect '/game_over' if WEB_PLAY.board.game_end?
+    current_player = WEB_PLAY.change_player
+    @message = "#{current_player.name} to play next"
+    erb :computer_vs_computer_form, :locals => LOCALS
 
   end
 
   post '/computer_vs_computer' do
     if params[:play] == 'Yes'
-      POSITION = CURRENT_PLAYER.determine_move(BOARD) 
-      GAME.make_move(BOARD, POSITION)
+      POSITION = WEB_PLAY.current_player.determine_move(WEB_PLAY.board) 
+      WEB_PLAY.game.make_move(WEB_PLAY.board, POSITION)
       redirect '/computer_vs_computer'
     else
       @message = "Game abandoned."
-      erb :game_over_form, :locals => {'board' => BOARD.board,'board_index' => BOARD.index_board, 'player1' => PLAYER1, 'player2' => PLAYER2, 'board_size' => BOARD_SIZE, 'win_size' => WIN_SIZE} 
+      erb :game_over_form, :locals => LOCALS
     end
   end
-
 
 end
