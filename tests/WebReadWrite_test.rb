b053c3f58cf04e90316af7492731a5a6a026a6d2
Jonathan Graham <jonathanpgraham@yahoo.co.uk>
2015-06-22 17:29:10 -0500

    Update WebReadWrite_test.rb
    Created a session setup class. Added tests for creation of new board and players.

--- tests/WebReadWrite_test.rb
+++ tests/WebReadWrite_test.rb
@@ -11,12 +11,22 @@ require '/Users/jonathangraham/TTT_TDD/lib/Human'
 require '/Users/jonathangraham/TTT_TDD/lib/Game'
 
 
-class WebReadWriteTest < Minitest::Test
+class CurrentUserSession < Minitest::Test
   include Rack::Test::Methods
-
+ 
   def app
     Sinatra::Application
   end
+
+  def new_setup
+    post'/new_game', {"player1" => "Human", "player1_name" => "test_name", "player1_level" => "Easy", 
+      "player2" => "Computer", "player2_name" => "Easy", "player2_level" => "Easy",
+      "board_size" => "3", "win_size" => "3"}
+  end
+
+end
+
+class WebReadWriteTest < CurrentUserSession
 
   def test_it_redirects_to_new_game_with_root_url 
     get '/'
@@ -25,7 +35,7 @@ class WebReadWriteTest < Minitest::Test
   end
 
   def test_new_game_opens_new_game_form
-    get '/new_game'
+    get '/new_game' 
     assert last_response.ok?
     assert last_response.body.include?('form action="/new_game" method="POST"')
     assert last_response.body.include?('If Human, type your name: <input type="text" name="player2_name">')
@@ -37,6 +47,18 @@ class WebReadWriteTest < Minitest::Test
     assert_equal "http://example.org/start_game", last_request.url
   end
 
+  def test_new_game_creates_new_board
+    new_setup
+    assert_equal [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',], last_request.session[:board].board
+  end
+
+  def test_new_game_creates_new_players
+    new_setup
+    assert_equal [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',], last_request.session[:board].board
+    assert_equal "test_name", last_request.session[:player1].name
+    assert_equal Human, last_request.session[:player1].class
+    assert_equal SequentialAI, last_request.session[:player2].class
+  end
 
   def test_it_redirects_to_new_game_when_play_again_selected 
     post '/game_over', {"play_again" => "Yes"}
