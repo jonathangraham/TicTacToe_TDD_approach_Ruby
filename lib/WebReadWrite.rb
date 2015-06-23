af81b7ce6fd7d7100e1d05a83f55ac02d44d2c24
Jonathan Graham <jonathanpgraham@yahoo.co.uk>
2015-06-23 13:38:49 -0500

    Update WebReadWrite.rb
    Refactored so computer_vs_computer logic is the same as the main game logic.
    More helper functions pulled out from game flow.
    Game_over message logic corrected so that win? called first (avoids a tie being called when a win is obtained on the last play of the board)
    Changed default board_size and win_size to 3

--- lib/WebReadWrite.rb
+++ lib/WebReadWrite.rb
@@ -4,11 +4,8 @@ require '/Users/jonathangraham/TTT_TDD/lib/RandomAI'
 require '/Users/jonathangraham/TTT_TDD/lib/NegamaxAI'
 require '/Users/jonathangraham/TTT_TDD/lib/Human'
 require '/Users/jonathangraham/TTT_TDD/lib/Game'
-#require '/Users/jonathangraham/TTT_TDD/lib/WebReadWrite'
 require 'sinatra'
 
-
-#class WebReadWrite < Sinatra::Base
 
   enable :sessions
 
@@ -24,8 +21,8 @@ require 'sinatra'
     player1_info = [params[:player1], params[:player1_name], params[:player1_level]]
     player2_info = [params[:player2], params[:player2_name], params[:player2_level]]
     players_info = [player1_info, player2_info]
-    board_size = [params[:board_size].to_i, 2].max
-    win_size = [params[:win_size].to_i, 2].max
+    board_size = [params[:board_size].to_i, 3].max
+    win_size = [params[:win_size].to_i, 3].max
     if win_size > board_size
       win_size = board_size
     end
@@ -44,54 +41,41 @@ require 'sinatra'
       'win_size' => session[:board].x_in_a_row
     }
 
-    redirect '/start_game'
+    redirect "/next_move"
   end
 
-  get '/start_game' do
-    if session[:player1].class != Human && session[:player2].class != Human
-      redirect '/computer_vs_computer'
-    else
-     redirect '/make_move'
-    end
-  end
-
-  get '/make_move' do
-    redirect '/game_over' if session[:board].game_end?   
+  get '/next_move' do
+    redirect '/game_over' if game_over?   
     change_player
-    if session[:current_player].class != Human
-      session[:position] = session[:current_player].determine_move(session[:board]) 
-      session[:game].make_move(session[:board], session[:position])
-      redirect '/make_move'
-    else
-      if session[:board].board.count(' ') == session[:board].board.length
-        @message = "First go! #{session[:current_player].name} to start."
-      else
-        @message = "Last play was in position #{session[:position]}. #{session[:current_player].name} take your turn."
-      end
-      erb :make_move_form, :locals => session[:locals]
-    end  
+    redirect "/#{session[:current_player].type}"
   end
 
-  post '/make_move' do
-    session[:position] = params[:move].to_i
-    if session[:board].valid_space?(session[:position]) == false
-      redirect '/try_again/'
-    else
-      session[:game].make_move(session[:board], session[:position])
-    end
-    redirect '/make_move'
+  get '/Human' do
+    @message = next_move_message
+    erb :human_form, :locals => session[:locals]
+  end
+
+  post '/Human' do
+    session[:position] = params[:move].to_i 
+    redirect '/try_again/' if not_valid_space? 
+    redirect '/play_move'
   end
 
   get '/try_again/' do
     @message = "Position #{session[:position]} is not valid. Choose an available space!"
-    erb :make_move_form, :locals => session[:locals]
+    erb :human_form, :locals => session[:locals]
+  end
+
+  get '/play_move' do
+    session[:game].make_move(session[:board], session[:position])
+    redirect 'next_move'
   end
 
   get '/game_over' do
-    if session[:board].tie?
-      @message = "Game was a tie. Would you like to play again?"
-    else
+    if win?
       @message = "#{session[:current_player].name} won! Would you like to play again?"
+    else
+      @message = "Game was a tie. Would you like to play again?"
     end
     erb :game_over_form, :locals => session[:locals]
   end
@@ -104,22 +88,18 @@ require 'sinatra'
     end
   end
 
-  get '/computer_vs_computer' do   
-    redirect '/game_over' if session[:board].game_end?
-    change_player
-    @message = "#{session[:current_player].name} to play next"
-    erb :computer_vs_computer_form, :locals => session[:locals]
-
+  get '/Computer' do   
+    @message = next_move_message
+    erb :computer_form, :locals => session[:locals]
   end
 
-  post '/computer_vs_computer' do
+  post '/Computer' do
     if params[:play] == 'Yes'
-      session[:position] = session[:current_player].determine_move(session[:board]) 
-      session[:game].make_move(session[:board], session[:position])
-      redirect '/computer_vs_computer'
+      session[:position] = session[:current_player].determine_move(session[:board])
+      redirect '/play_move'
     else
       @message = "Game abandoned."
-      erb :game_over_form, :locals => LOCALS
+      erb :game_over_form, :locals => session[:locals]
     end
   end
 
@@ -131,14 +111,19 @@ require 'sinatra'
 
     def player_setup(players_info, number)
       players = players_info.map do |player|
-        if player[0] == 'Human'
-          Human.new(player[1])
-        elsif player[2] == 'Easy'
-          SequentialAI.new('Computer_Easy')
-        elsif player[2] == 'Moderate'
-          RandomAI.new('Computer_Moderate')
-        elsif player[2] == 'Hard'
-          NegamaxAI.new('Computer_Hard')
+        type = player[0]
+        name = player[1]
+        level = player[2]
+        if type == 'Human'
+          Human.new(name)
+        else
+          if level == 'Easy'
+            SequentialAI.new("AI(#{level})")
+          elsif level == 'Moderate'
+            RandomAI.new("AI(#{level})")
+          elsif level == 'Hard'
+            NegamaxAI.new("AI(#{level})")
+          end
         end
       end
       players[number]
@@ -156,8 +141,29 @@ require 'sinatra'
       end
     end
 
+    def game_over?
+      session[:board].game_end? 
+    end
+
+    def next_move_message
+      if new_board? 
+        "First go! #{session[:current_player].name} to start."
+      else
+        "Last play was in position #{session[:position]}. #{session[:current_player].name} take your turn."
+      end
+    end
+
+    def new_board?
+      session[:board].board.count(' ') == session[:board].board.length
+    end
+
+    def not_valid_space?
+      session[:board].valid_space?(session[:position]) == false
+    end
+
+    def win?
+      marker_played = session[:board].marker_just_played
+      session[:board].win?(marker_played)
+    end
+
   end
-
-#end
-
-
