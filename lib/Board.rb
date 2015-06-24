7884305d1b068c6dbb4816607fae81e2dedf505f
Jonathan Graham <jonathanpgraham@yahoo.co.uk>
2015-06-24 14:17:08 -0500

    Update Board.rb
    Defined methods only called by self as private.
    Removed function empty_space?, as duplicated in valid_space?
    Added grid_size to board methods

--- lib/Board.rb
+++ lib/Board.rb
@@ -1,7 +1,8 @@
 class Board
-	attr_reader :board, :index_board, :x_in_a_row
+	attr_reader :board, :index_board, :grid_size, :x_in_a_row
 
 	def initialize(grid_size, x_in_a_row)
+		@grid_size = grid_size
 		elements = grid_size * grid_size
 		@board = Array.new(elements, ' ')
 		@index_board = Array(0..(elements - 1))
@@ -12,96 +13,11 @@ class Board
 		@board[position] = letter
 	end
 
-	def empty_space?(position)
-		@board[position] == ' '
-	end
-
-	def tie?()
-		@board.count(' ') == 0
-	end
-
-	def rows()
-		rows = @board.each_slice(Math.sqrt(@board.length)).to_a
-	end
-
-	def columns()
-		rows.transpose
-	end
-
-	def diaganols()
-		n = rows.length
-		diaganols = [right_diaganols(n), left_diaganols(n)].flatten(1)
-	end
-
-	def right_diaganols(n)
-		right_diaganols = []
-		row = 0
-		until row == n do
-			column = 0
-			until column == n do
-				right_diaganols << right_diaganol(row, column, n)
-				column = column + 1
-			end
-			row = row + 1
-		end
-		right_diaganols
-	end
-
-	def right_diaganol(row, column, n)
-		right_diaganol = []
-		until row == n || column == n do
-			right_diaganol << rows[row][column]
-			row = row + 1
-			column = column + 1
-		end
-		right_diaganol
-	end
-
-	def left_diaganols(n)
-		left_diaganols = []
-		row = 0
-		until row == n do
-			column = 0
-			until column == n do
-				left_diaganols << left_diaganol(row, column, n)
-				column = column + 1
-			end
-			row = row + 1
-		end
-		left_diaganols
-	end
-
-	def left_diaganol(row, column, n)
-		left_diaganol = []
-		until row == n || column < 0 do
-			left_diaganol << rows[row][column]
-			row = row + 1
-			column = column - 1
-		end
-		left_diaganol
-	end
-
-	def potential_winning_lines()
-		rows | columns | diaganols 
-	end
-
 	def win?(marker)
 		checked_lines = potential_winning_lines().map do |innerArray|
 			winning_line?(innerArray, marker).include? true
 		end
 		checked_lines.include? true
-	end
-
-	def winning_line?(innerArray, marker)
-		arr_booleans = []
-		innerArray.map do |e|
-			if e == marker
-				arr_booleans << true
-			else
-				arr_booleans = []
-			end
-			arr_booleans.length >= @x_in_a_row
-		end
 	end
 
 	def game_end?()
@@ -128,4 +44,88 @@ class Board
 	def valid_space?(position)
 		@board[position] == ' ' && @index_board.include?(position)
 	end
+
+
+	private
+
+		def rows()
+			rows = @board.each_slice(Math.sqrt(@board.length)).to_a
+		end
+
+		def columns()
+			rows.transpose
+		end
+
+		def diaganols()
+			n = rows.length
+			diaganols = [right_diaganols(n), left_diaganols(n)].flatten(1)
+		end
+
+		def right_diaganols(n)
+			right_diaganols = []
+			row = 0
+			until row == n do
+				column = 0
+				until column == n do
+					right_diaganols << right_diaganol(row, column, n)
+					column = column + 1
+				end
+				row = row + 1
+			end
+			right_diaganols
+		end
+
+		def right_diaganol(row, column, n)
+			right_diaganol = []
+			until row == n || column == n do
+				right_diaganol << rows[row][column]
+				row = row + 1
+				column = column + 1
+			end
+			right_diaganol
+		end
+
+		def left_diaganols(n)
+			left_diaganols = []
+			row = 0
+			until row == n do
+				column = 0
+				until column == n do
+					left_diaganols << left_diaganol(row, column, n)
+					column = column + 1
+				end
+				row = row + 1
+			end
+			left_diaganols
+		end
+
+		def left_diaganol(row, column, n)
+			left_diaganol = []
+			until row == n || column < 0 do
+				left_diaganol << rows[row][column]
+				row = row + 1
+				column = column - 1
+			end
+			left_diaganol
+		end
+
+		def potential_winning_lines()
+			rows | columns | diaganols 
+		end
+
+		def winning_line?(innerArray, marker)
+			arr_booleans = []
+			innerArray.map do |e|
+				if e == marker
+					arr_booleans << true
+				else
+					arr_booleans = []
+				end
+				arr_booleans.length >= @x_in_a_row
+			end
+		end
+
+		def tie?()
+			@board.count(' ') == 0
+		end
 end
