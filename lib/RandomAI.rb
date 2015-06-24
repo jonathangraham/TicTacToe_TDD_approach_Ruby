374aaa7da5d4a191fd578a3b9b43285c94129cc2
Jonathan Graham <jonathanpgraham@yahoo.co.uk>
2015-06-24 14:21:51 -0500

    Update RandomAI.rb
    In determine_move, changed call to board.empty_space? to board.valid_space? and removed call to board.tie? (was unnecessary as determine_move will only be called if game is not over).
    Moved generate_random_position to a private method.

--- lib/RandomAI.rb
+++ lib/RandomAI.rb
@@ -7,15 +7,17 @@ class RandomAI
 		@type = 'Computer'
 	end
 
-	def generate_random_position(board)
-		rand(board.board.length)
-	end
-
 	def determine_move(board)
 		position = generate_random_position(board)
-		until board.empty_space?(position) || board.tie? do
+		until board.valid_space?(position) do
 			position = generate_random_position(board)
 		end
 		position
 	end
+
+	private
+
+		def generate_random_position(board)
+			rand(board.board.length)
+		end
 end
