3ee039b3917277a3994eb820d2c7a09dd193570c
Jonathan Graham <jonathanpgraham@yahoo.co.uk>
2015-06-26 11:57:19 -0500

    Update Human.rb
    Subclasses of Human for Console and Web play.
    Human superclass initialises now with the interface and player marker, as well as a default position for the move, which is set to be invalid for all boards. 
    Subclasses have public method determine_move(board).

--- lib/Human.rb
+++ lib/Human.rb
@@ -1,10 +1,28 @@
 class Human
 
-	attr_accessor :name, :type
+	attr_accessor :name, :interface, :position, :marker
 
-	def initialize(name)
+	def initialize(interface, name, marker)
 		@name = name
-		@type = 'Human'
+		@interface = interface
+		@marker = marker
+		@position = -1
+	end
+
+end
+
+class ConsoleHuman < Human
+
+	def determine_move(board)
+		interface.get_human_move()
 	end
 	
+end
+
+class WebHuman < Human
+
+	def determine_move(board)
+		@position
+	end
+
 end
