260d9da4a9d170241d5e5a00afb1346202da60b3
Jonathan Graham <jonathanpgraham@yahoo.co.uk>
2015-06-26 12:12:28 -0500

    Update SequentialAI.rb
    Now as a subclass in AI.rb

--- lib/SequentialAI.rb
+++ lib/SequentialAI.rb
@@ -1,13 +1,1 @@
-class SequentialAI
 
-	attr_reader :name, :type
-
-	def initialize(name)
-		@name = name
-		@type = 'Computer'
-	end
-
-	def determine_move(board)
-		board.available_spaces.first
-	end
-end
