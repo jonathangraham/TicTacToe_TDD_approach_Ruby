409a0029bb6aecc3d8e05244694fedffb64af2e6
Jonathan Graham <sendthistojon@gmail.com>
2015-06-29 13:59:54 -0500

    deleted commented out tests

--- tests/Game_test.rb
+++ tests/Game_test.rb
@@ -15,13 +15,16 @@ class TestGame < Minitest::Test
 
 	def test_initialization_of_new_game
 		game = Game.new('interface', 3, 3, ['ConsoleHuman', 'jon', ""], ['Computer', "", "2"])
-		assert_equal([" ", " ", " ", " ", " ", " ", " ", " ", " "], game.board.board)
-		assert_equal(ConsoleHuman, game.player1.class)
-		assert_equal('jon', game.player1.name)
-		assert_equal('X', game.player1.marker)
-		assert_equal(RandomAI, game.player2.class)
-		assert_equal('AI (level 2)', game.player2.name)
-		assert_equal('O', game.player2.marker)
+		board = Board.new(3,3)
+		player1 = ConsoleHuman.new('interface', 'jon', 'X')
+		player2 = RandomAI.new('AI (level 2)', 'O')
+		assert_equal(board.board, game.board.board)
+		assert_equal(player1.class, game.player1.class)
+		assert_equal(player1.name, game.player1.name)
+		assert_equal(player1.marker, game.player1.marker)
+		assert_equal(player2.class, game.player2.class)
+		assert_equal(player2.name, game.player2.name)
+		assert_equal(player2.marker, game.player2.marker)
 	end
 
 	def test_determine_current_player_new_board
@@ -35,73 +38,6 @@ class TestGame < Minitest::Test
 		replace_initial_board_for_tests(board, ['X', 'O', 'X', ' ', ' ', ' ', ' ', ' ', ' '])
 		assert_equal(game.player2, game.determine_current_player)
 	end
-
-end
 	
-# 	def test_sequential_game_move_determination
-# 		board = Board.new(3, 3)
-# 		player1 = SequentialAI.new('Comp')
-# 		player2 = SequentialAI.new('Comp')
-# 		replace_initial_board_for_tests(board, ['X', 'O', 'X', ' ', ' ', ' ', ' ', ' ', ' '])
-# 		mock_console_read_write = MockConsoleReadWrite.new
-# 		ui = UI.new(mock_console_read_write)
-# 		game = Game.new(board, player1, player2)
-# 		assert_equal(3, game.generate_move(player2))
-# 	end
+end
 
-# 	def test_random_game_move_determination
-# 		board = Board.new(3, 3)
-# 		player1 = RandomAI.new('Comp')
-# 		player2 = RandomAI.new('Comp')
-# 		replace_initial_board_for_tests(board, ['X', 'O', 'X', ' ', ' ', 'O', 'X', 'O', 'X'])
-# 		mock_console_read_write = MockConsoleReadWrite.new
-# 		ui = UI.new(mock_console_read_write)
-# 		game = Game.new(board, player1, player2)
-# 		assert_equal(true, [3,4].include?(game.generate_move(player2)))
-# 	end
-
-# 	def test_negamax_game_move_determination
-# 		board = Board.new(3, 3)
-# 		player1 = RandomAI.new('Comp')
-# 		player2 = NegamaxAI.new('Comp')
-# 		replace_initial_board_for_tests(board, [' ', ' ', 'X', ' ', 'O', ' ', 'X', ' ', ' '])
-# 		mock_console_read_write = MockConsoleReadWrite.new
-# 		ui = UI.new(mock_console_read_write)
-# 		game = Game.new(board, player1, player2)
-# 		assert_equal(1, game.generate_move(player2))
-# 	end
-
-# 	def test_make_move
-# 		board = Board.new(3, 3)
-# 		player1 = SequentialAI.new('Comp')
-# 		player2 = SequentialAI.new('Comp')
-# 		replace_initial_board_for_tests(board, ['X', 'O', 'X', 'O', ' ', ' ', ' ', ' ', ' '])
-# 		mock_console_read_write = MockConsoleReadWrite.new
-# 		ui = UI.new(mock_console_read_write)
-# 		game = Game.new(board, player1, player2)
-# 		position = game.generate_move(player1)
-# 		game.make_move(board, position)
-# 		assert_equal(['X', 'O', 'X', 'O', 'X', ' ', ' ', ' ', ' '], board.board)
-# 	end	
-
-# end
-
-
-# class MockConsoleReadWrite
-# 	attr_accessor :lines_printed
-# 	attr_accessor :lines_to_read
-
-# 	def initialize
-# 		@lines_printed =[]
-# 		@lines_to_read = []
-# 	end
-
-# 	def print_line(str)
-# 		@lines_printed << str
-# 	end
-
-# 	def read_line
-# 		str = "read_line called"
-# 		@lines_to_read << str
-# 	end
-# end
